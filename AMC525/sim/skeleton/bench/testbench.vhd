77db8309a313257c7c59c8d4e299aefc44c91239
Michael Abbott <michael.abbott@diamond.ac.uk>
2016-07-06 11:23:20 +0100

    Add simple vsim skeleton
    This runs up vsim as appropriate with a trivial testbench.

---
+++ AMC525/sim/skeleton/bench/testbench.vhd
@@ -0,0 +1,45 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity testbench is
+end testbench;
+
+
+library work;
+use work.defines.all;
+
+architecture testbench of testbench is
+    procedure clk_wait(signal clk_i : in std_logic; count : in natural) is
+        variable i : natural;
+    begin
+        for i in 0 to count-1 loop
+            wait until rising_edge(clk_i);
+        end loop;
+    end procedure;
+
+
+    signal dsp_clk : STD_LOGIC := '0';
+    signal dsp_reset_n : STD_LOGIC := '0';
+
+
+
+    procedure tick_wait(count : natural) is
+    begin
+        clk_wait(dsp_clk, count);
+    end procedure;
+
+    procedure tick_wait is
+    begin
+        clk_wait(dsp_clk, 1);
+    end procedure;
+
+begin
+
+    dsp_reset_n <= '1' after 50 ns;
+    dsp_clk <= not dsp_clk after 2 ns;
+
+
+
+
+end testbench;
---
+++ AMC525/sim/skeleton/do/compile.do
@@ -0,0 +1,32 @@
+# Paths from environment
+set vhd_dir $env(VHD_DIR)
+set bench_dir $env(BENCH_DIR)
+
+vlib work
+vlib msim
+vlib msim/xil_defaultlib
+
+vcom -64 -93 -work xil_defaultlib \
+    $vhd_dir/support.vhd
+
+vcom -64 -93 -work xil_defaultlib \
+    $bench_dir/testbench.vhd
+
+
+# compile glbl module
+vlog -work xil_defaultlib $bench_dir/glbl.v
+
+vsim -t 1ps \
+    -pli "/dls_sw/FPGA/Xilinx/Vivado/2015.1/lib/lnx64.o/libxil_vsim.so" \
+    -lib xil_defaultlib testbench
+
+view wave
+
+add wave -group "Top" sim:*
+add wave sim:*
+# add wave sim:/testbench/axi_burst_master_inst/*
+
+
+run 1us
+
+# vim: set filetype=tcl:
