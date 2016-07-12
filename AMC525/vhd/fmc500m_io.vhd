4b09166156b8ddd261ce3e3f59c1b8b3f5dcfe52
Michael Abbott <michael.abbott@diamond.ac.uk>
2016-07-12 08:31:44 +0100

    Also sort out the FMC500 IO definitions to work within VHDL's limits

--- AMC525/vhd/fmc500m_io.vhd
+++ AMC525/vhd/fmc500m_io.vhd
@@ -79,6 +79,11 @@ entity fmc500m_io is
 end;
 
 architecture fmc500m_io of fmc500m_io is
+    signal adc_data_p : std_logic_vector(13 downto 0);
+    signal adc_data_n : std_logic_vector(13 downto 0);
+    signal dac_data_p : std_logic_vector(15 downto 0);
+    signal dac_data_n : std_logic_vector(15 downto 0);
+
 begin
     -- Unused pins
     FMC_HB_P(2) <= 'Z';     -- dac_ext_sync, unused input
@@ -95,46 +100,46 @@ begin
     -- -------------------------------------------------------------------------
     -- PLL
 
-    -- SPI outputs
-    pll_spi_obuf_inst : entity work.obuf_array generic map (
-        COUNT => 3
-    ) port map (
+    -- SPI
+    pll_spi_csn_inst : entity work.obuf_array port map (
         i_i(0) => pll_spi_csn_i,
-        o_o(0) => FMC_LA_N(28),
-
-        i_i(1) => pll_spi_sclk_i,
-        o_o(1) => FMC_LA_P(28),
-
-        i_i(2) => pll_spi_sdi_i,
-        o_o(2) => FMC_LA_P(22)
+        o_o(0) => FMC_LA_N(28)
     );
-    -- SPI input
+    pll_spi_sclk_inst : entity work.obuf_array port map (
+        i_i(0) => pll_spi_sclk_i,
+        o_o(0) => FMC_LA_P(28)
+    );
+    pll_spi_sdi_inst : entity work.obuf_array port map (
+        i_i(0) => pll_spi_sdi_i,
+        o_o(0) => FMC_LA_P(22)
+    );
     pll_spi_sdo_inst : entity work.ibuf_array port map (
         i_i(0) => FMC_LA_N(22),
         o_o(0) => pll_spi_sdo_o
     );
+
     -- Status inputs
-    pll_status_inst : entity work.ibuf_array generic map (
-        COUNT => 2
-    ) port map (
+    pll_status_ld1_inst : entity work.ibuf_array port map (
         i_i(0) => FMC_LA_P(29),
-        o_o(0) => pll_status_ld1_o,
-
-        i_i(1) => FMC_LA_N(29),
-        o_o(1) => pll_status_ld2_o
+        o_o(0) => pll_status_ld1_o
     );
+    pll_status_ld2_inst : entity work.ibuf_array port map (
+        i_i(0) => FMC_LA_N(29),
+        o_o(0) => pll_status_ld2_o
+    );
+
     -- Miscellaneous control outputs
-    pll_misc_obuf_inst : entity work.obuf_array generic map (
-        COUNT => 3
-    ) port map (
+    pll_clkin_sel0_inst : entity work.obuf_array port map (
         i_i(0) => pll_clkin_sel0_i,
-        o_o(0) => FMC_LA_P(31),
-
-        i_i(1) => pll_clkin_sel1_i,
-        o_o(1) => FMC_LA_N(31),
-
-        i_i(2) => pll_sync_i,
-        o_o(2) => FMC_LA_P(18)
+        o_o(0) => FMC_LA_P(31)
+    );
+    pll_clkin_sel1_inst : entity work.obuf_array port map (
+        i_i(0) => pll_clkin_sel1_i,
+        o_o(0) => FMC_LA_N(31)
+    );
+    pll_sync_inst : entity work.obuf_array port map (
+        i_i(0) => pll_sync_i,
+        o_o(0) => FMC_LA_P(18)
     );
 
     -- Clocks
@@ -158,60 +163,65 @@ begin
         p_i(0) => FMC_LA_P(0),      n_i(0) => FMC_LA_N(0),
         o_o(0) => adc_dco_o
     );
+
     -- Data buffer from ADC
+    adc_data_p(0)  <= FMC_LA_P(7);      adc_data_n(0)  <= FMC_LA_N(7);
+    adc_data_p(1)  <= FMC_LA_P(2);      adc_data_n(1)  <= FMC_LA_N(2);
+    adc_data_p(2)  <= FMC_LA_P(1);      adc_data_n(2)  <= FMC_LA_N(1);
+    adc_data_p(3)  <= FMC_LA_P(3);      adc_data_n(3)  <= FMC_LA_N(3);
+    adc_data_p(4)  <= FMC_LA_P(4);      adc_data_n(4)  <= FMC_LA_N(4);
+    adc_data_p(5)  <= FMC_LA_P(5);      adc_data_n(5)  <= FMC_LA_N(5);
+    adc_data_p(6)  <= FMC_LA_P(8);      adc_data_n(6)  <= FMC_LA_N(8);
+    adc_data_p(7)  <= FMC_LA_P(9);      adc_data_n(7)  <= FMC_LA_N(9);
+    adc_data_p(8)  <= FMC_LA_P(12);     adc_data_n(8)  <= FMC_LA_N(12);
+    adc_data_p(9)  <= FMC_LA_P(11);     adc_data_n(9)  <= FMC_LA_N(11);
+    adc_data_p(10) <= FMC_LA_P(16);     adc_data_n(10) <= FMC_LA_N(16);
+    adc_data_p(11) <= FMC_LA_P(15);     adc_data_n(11) <= FMC_LA_N(15);
+    adc_data_p(12) <= FMC_LA_P(20);     adc_data_n(12) <= FMC_LA_N(20);
+    adc_data_p(13) <= FMC_LA_P(13);     adc_data_n(13) <= FMC_LA_N(13);
     adc_data_inst : entity work.ibufds_array generic map (
         COUNT => 14
     ) port map (
-        p_i(0)  => FMC_LA_P(7),     n_i(0)  => FMC_LA_N(7),
-        p_i(1)  => FMC_LA_P(2),     n_i(1)  => FMC_LA_N(2),
-        p_i(2)  => FMC_LA_P(1),     n_i(2)  => FMC_LA_N(1),
-        p_i(3)  => FMC_LA_P(3),     n_i(3)  => FMC_LA_N(3),
-        p_i(4)  => FMC_LA_P(4),     n_i(4)  => FMC_LA_N(4),
-        p_i(5)  => FMC_LA_P(5),     n_i(5)  => FMC_LA_N(5),
-        p_i(6)  => FMC_LA_P(8),     n_i(6)  => FMC_LA_N(8),
-        p_i(7)  => FMC_LA_P(9),     n_i(7)  => FMC_LA_N(9),
-        p_i(8)  => FMC_LA_P(12),    n_i(8)  => FMC_LA_N(12),
-        p_i(9)  => FMC_LA_P(11),    n_i(9)  => FMC_LA_N(11),
-        p_i(10) => FMC_LA_P(16),    n_i(10) => FMC_LA_N(16),
-        p_i(11) => FMC_LA_P(15),    n_i(11) => FMC_LA_N(15),
-        p_i(12) => FMC_LA_P(20),    n_i(12) => FMC_LA_N(20),
-        p_i(13) => FMC_LA_P(13),    n_i(13) => FMC_LA_N(13),
+        p_i => adc_data_p,
+        n_i => adc_data_n,
         o_o => adc_data_o
     );
+
     -- ADC overflow detect signal
     adc_status_inst : entity work.ibufds_array port map (
         p_i(0)  => FMC_LA_P(14),    n_i(0)  => FMC_LA_N(14),
         o_o(0) => adc_status_o
     );
     -- Fast detect inputs
-    adc_fd_inst : entity work.ibuf_array generic map (
-        COUNT => 2
-    ) port map (
+    adc_fd_a_inst : entity work.ibuf_array port map (
         i_i(0) => FMC_LA_N(18),
-        o_o(0) => adc_fd_a_o,
-
-        i_i(1) => FMC_LA_P(6),
-        o_o(1) => adc_fd_b_o
+        o_o(0) => adc_fd_a_o
     );
-    -- SPI bidirectional SDIO
+    adc_fd_b_inst : entity work.ibuf_array port map (
+        i_i(0) => FMC_LA_P(6),
+        o_o(0) => adc_fd_b_o
+    );
+
+    -- SPI
     adc_spi_sdio_inst : entity work.iobuf_array port map (
         o_o(0) => adc_spi_sdio_o,
         i_i(0) => adc_spi_sdio_i,
         en_i(0) => adc_spi_sdio_en_i,
         io(0) => FMC_LA_N(6)
     );
-    -- Remaining SPI and power enable
-    adc_misc_out_inst : entity work.obuf_array generic map (
-        COUNT => 3
-    ) port map (
+    adc_spi_csn_inst : entity work.obuf_array port map (
         i_i(0) => adc_spi_csn_i,
-        o_o(0) => FMC_LA_N(10),
+        o_o(0) => FMC_LA_N(10)
+    );
+    adc_spi_sclk_inst : entity work.obuf_array port map (
+        i_i(0) => adc_spi_sclk_i,
+        o_o(0) => FMC_LA_P(10)
+    );
 
-        i_i(1) => adc_spi_sclk_i,
-        o_o(1) => FMC_LA_P(10),
-
-        i_i(2) => adc_pdwn_i,
-        o_o(2) => FMC_LA_P(25)
+    -- ADC power down control
+    adc_pdwn_inst : entity work.obuf_array port map (
+        i_i(0) => adc_pdwn_i,
+        o_o(0) => FMC_LA_P(25)
     );
 
 
@@ -219,63 +229,67 @@ begin
     -- DAC
 
     -- Data buffer to DAC
-    dac_d_inst : entity work.obufds_array generic map (
+    FMC_HB_P(17) <= dac_data_p(0);      FMC_HB_N(17) <= dac_data_n(0);
+    FMC_HB_P(19) <= dac_data_p(1);      FMC_HB_N(19) <= dac_data_n(1);
+    FMC_HB_P(18) <= dac_data_p(2);      FMC_HB_N(18) <= dac_data_n(2);
+    FMC_HB_P(15) <= dac_data_p(3);      FMC_HB_N(15) <= dac_data_n(3);
+    FMC_HB_P(21) <= dac_data_p(4);      FMC_HB_N(21) <= dac_data_n(4);
+    FMC_HB_P(14) <= dac_data_p(5);      FMC_HB_N(14) <= dac_data_n(5);
+    FMC_HB_P(10) <= dac_data_p(6);      FMC_HB_N(10) <= dac_data_n(6);
+    FMC_HB_P(13) <= dac_data_p(7);      FMC_HB_N(13) <= dac_data_n(7);
+    FMC_HB_P(11) <= dac_data_p(8);      FMC_HB_N(11) <= dac_data_n(8);
+    FMC_HB_P(8)  <= dac_data_p(9);      FMC_HB_N(8)  <= dac_data_n(9);
+    FMC_HB_P(9)  <= dac_data_p(10);     FMC_HB_N(9)  <= dac_data_n(10);
+    FMC_HB_P(7)  <= dac_data_p(11);     FMC_HB_N(7)  <= dac_data_n(11);
+    FMC_HB_P(6)  <= dac_data_p(12);     FMC_HB_N(6)  <= dac_data_n(12);
+    FMC_HB_P(5)  <= dac_data_p(13);     FMC_HB_N(5)  <= dac_data_n(13);
+    FMC_HB_P(1)  <= dac_data_p(14);     FMC_HB_N(1)  <= dac_data_n(14);
+    FMC_HB_P(3)  <= dac_data_p(15);     FMC_HB_N(3)  <= dac_data_n(15);
+    dac_data_inst : entity work.obufds_array generic map (
         COUNT => 16
     ) port map (
         i_i => dac_data_i,
-        p_o(0)  => FMC_HB_P(17),    n_o(0)  => FMC_HB_N(17),
-        p_o(1)  => FMC_HB_P(19),    n_o(1)  => FMC_HB_N(19),
-        p_o(2)  => FMC_HB_P(18),    n_o(2)  => FMC_HB_N(18),
-        p_o(3)  => FMC_HB_P(15),    n_o(3)  => FMC_HB_N(15),
-        p_o(4)  => FMC_HB_P(21),    n_o(4)  => FMC_HB_N(21),
-        p_o(5)  => FMC_HB_P(14),    n_o(5)  => FMC_HB_N(14),
-        p_o(6)  => FMC_HB_P(10),    n_o(6)  => FMC_HB_N(10),
-        p_o(7)  => FMC_HB_P(13),    n_o(7)  => FMC_HB_N(13),
-        p_o(8)  => FMC_HB_P(11),    n_o(8)  => FMC_HB_N(11),
-        p_o(9)  => FMC_HB_P(8),     n_o(9)  => FMC_HB_N(8),
-        p_o(10) => FMC_HB_P(9),     n_o(10) => FMC_HB_N(9),
-        p_o(11) => FMC_HB_P(7),     n_o(11) => FMC_HB_N(7),
-        p_o(12) => FMC_HB_P(6),     n_o(12) => FMC_HB_N(6),
-        p_o(13) => FMC_HB_P(5),     n_o(13) => FMC_HB_N(5),
-        p_o(14) => FMC_HB_P(1),     n_o(14) => FMC_HB_N(1),
-        p_o(15) => FMC_HB_P(3),     n_o(15) => FMC_HB_N(3)
+        p_o => dac_data_p,
+        n_o => dac_data_n
     );
+
     -- Miscellaneous differential DAC outputs
-    dac_misc_obufds_inst : entity work.obufds_array generic map (
-        COUNT => 2
-    ) port map (
+    dac_dci_inst : entity work.obufds_array port map (
         i_i(0) => dac_dci_i,
-        p_o(0) => FMC_HB_P(16),     n_o(0) => FMC_HB_N(16),
-
-        i_i(1) => dac_frame_i,
-        p_o(1) => FMC_HB_P(12),     n_o(1) => FMC_HB_N(12)
+        p_o(0) => FMC_HB_P(16),     n_o(0) => FMC_HB_N(16)
     );
-    -- SPI and miscellaneous outputs
+    dac_frame_inst : entity work.obufds_array port map (
+        i_i(0) => dac_frame_i,
+        p_o(0) => FMC_HB_P(12),     n_o(0) => FMC_HB_N(12)
+    );
+
+    -- SPI
     -- Note that we operate the DAC SDIO pin as an SDI pin
-    dac_misc_obuf_inst : entity work.obuf_array generic map (
-        COUNT => 4
-    ) port map (
+    dac_spi_csn_inst : entity work.obuf_array port map (
         i_i(0) => dac_spi_csn_i,
-        o_o(0) => FMC_LA_N(33),
-
-        i_i(1) => dac_rstn_i,
-        o_o(1) => FMC_LA_P(33),
-
-        i_i(2) => dac_spi_sclk_i,
-        o_o(2) => FMC_HB_P(4),
-
-        i_i(3) => dac_spi_sdi_i,
-        o_o(3) => FMC_HB_N(20)
+        o_o(0) => FMC_LA_N(33)
     );
-    -- SPI and miscellaneous intuts
-    dac_misc_ibuf_inst : entity work.ibuf_array generic map (
-        COUNT => 2
-    ) port map (
+    dac_spi_sclk_inst : entity work.obuf_array port map (
+        i_i(0) => dac_spi_sclk_i,
+        o_o(0) => FMC_HB_P(4)
+    );
+    dac_spi_sdi_inst : entity work.obuf_array port map (
+        i_i(0) => dac_spi_sdi_i,
+        o_o(0) => FMC_HB_N(20)
+    );
+    dac_spi_sdo_inst : entity work.ibuf_array port map (
         i_i(0) => FMC_HB_P(20),
-        o_o(0) => dac_spi_sdo_o,
+        o_o(0) => dac_spi_sdo_o
+    );
 
-        i_i(1) => FMC_HB_N(4),
-        o_o(1) => dac_irqn_o
+    -- Miscellanous
+    dac_rstn_inst : entity work.obuf_array port map (
+        i_i(0) => dac_rstn_i,
+        o_o(0) => FMC_LA_P(33)
+    );
+    dac_irqn_inst : entity work.ibuf_array port map (
+        i_i(0) => FMC_HB_N(4),
+        o_o(0) => dac_irqn_o
     );
 
 
@@ -283,29 +297,31 @@ begin
     -- Miscellaneous
 
     -- Custom power management controllers
-    power_en_inst : entity work.obuf_array generic map (
-        COUNT => 3
-    ) port map (
-        i_i(0) => adc_pwr_en_i,
-        o_o(0) => FMC_LA_P(26),
-
-        i_i(1) => dac_pwr_en_i,
-        o_o(1) => FMC_LA_P(32),
-
-        i_i(2) => pll_pwr_en_i,
-        o_o(2) => FMC_LA_P(30)
+    pll_pwr_en_inst : entity work.obuf_array port map (
+        i_i(0) => pll_pwr_en_i,
+        o_o(0) => FMC_LA_P(30)
     );
-    power_good_inst : entity work.ibuf_array generic map (
-        COUNT => 3
-    ) port map (
+    pll_pwr_good_inst : entity work.ibuf_array port map (
+        i_i(0) => FMC_LA_P(27),
+        o_o(0) => pll_pwr_good_o
+    );
+
+    adc_pwr_en_inst : entity work.obuf_array port map (
+        i_i(0) => adc_pwr_en_i,
+        o_o(0) => FMC_LA_P(26)
+    );
+    adc_pwr_good_inst : entity work.ibuf_array port map (
         i_i(0) => FMC_LA_N(26),
-        o_o(0) => adc_pwr_good_o,
+        o_o(0) => adc_pwr_good_o
+    );
 
-        i_i(1) => FMC_LA_N(32),
-        o_o(1) => dac_pwr_good_o,
-
-        i_i(2) => FMC_LA_P(27),
-        o_o(2) => pll_pwr_good_o
+    dac_pwr_en_inst : entity work.obuf_array port map (
+        i_i(0) => dac_pwr_en_i,
+        o_o(0) => FMC_LA_P(32)
+    );
+    dac_pwr_good_inst : entity work.ibuf_array port map (
+        i_i(0) => FMC_LA_N(32),
+        o_o(0) => dac_pwr_good_o
     );
 
     -- External trigger input
