22973ecfd166fe3e50e39d24609f795731dc7595
Michael Abbott <michael.abbott@diamond.ac.uk>
2016-07-20 11:27:17 +0100

    Redo driver.py interface and update spi script to use new API
    Unfortunately some other scripts will have been broken at this point.

--- tools/driver.py
+++ tools/driver.py
@@ -12,25 +12,73 @@ REGS = '/dev/amc525_lmbf.0.reg'
 LMBF_MAP_SIZE = ord('L') << 8           # _IO('L', 0)
 LMBF_BUF_SIZE = (ord('L') << 8) | 1     # _IO('L', 1)
 
+# Register map parameters.  These must match definitions in vhd/defines.vhd
+MOD_ADDR_COUNT = 2**4
+REG_ADDR_COUNT = 2**5
+
+
 # Interrogate DMA buffer size
 ddr0_file = os.open(DDR0, os.O_RDONLY)
 DDR0_BUF_LEN = fcntl.ioctl(ddr0_file, LMBF_BUF_SIZE)
 os.close(ddr0_file)
 
-# Open register file and map into memory.
-reg_file = os.open(REGS, os.O_RDWR | os.O_SYNC)
-reg_size = fcntl.ioctl(reg_file, LMBF_MAP_SIZE)
-map = mmap.mmap(reg_file, reg_size)
-regs = numpy.frombuffer(map, dtype = numpy.uint32)
 
-# Register space is currently structured as 16 modules of 32 registers each.
-regs = regs[:16*32].reshape((16, 32))
 
-mem_gen_regs = regs[0]
-misc_regs = regs[2]
-debug_regs = regs[15]
+class Regs:
+    def __init__(self, regs_device):
+        # Open register file and map into memory.
+        self.reg_file = os.open(regs_device, os.O_RDWR | os.O_SYNC)
+        reg_size = fcntl.ioctl(self.reg_file, LMBF_MAP_SIZE)
+        self.reg_map = mmap.mmap(self.reg_file, reg_size)
+        regs = numpy.frombuffer(self.reg_map, dtype = numpy.uint32)
 
-__all__ = [
-    'DDR0', 'DDR1',
-    'DDR0_BUF_LEN',
-    'regs', 'mem_gen_regs', 'misc_regs', 'debug_regs']
+        # Reshape register space into individual modules
+        regs = regs[:MOD_ADDR_COUNT * REG_ADDR_COUNT]
+        regs = regs.reshape((MOD_ADDR_COUNT, REG_ADDR_COUNT))
+
+
+        self.mem_gen = regs[0]
+        self.dig_io  = regs[1]
+        self.fmc500  = regs[2]
+        self.debug   = regs[15]
+
+        self.spi_reg = self.fmc500[0:1]
+
+    def spi_read(self, dev, addr):
+        dev &= 3
+        addr &= 0xFFF
+        self.spi_reg[0] = 0x80000000 | (dev << 29) | (addr << 8)
+        return self.spi_reg[0]
+
+    def spi_write(self, dev, addr, data):
+        dev &= 3
+        addr &= 0xFFF
+        data &= 0xFF
+        self.spi_reg[0] = (dev << 29) | (addr << 8) | data
+        # Must read back to ensure that write has completed
+        resp = self.spi_reg[0]
+
+
+class SPI:
+    def __init__(self, regs, dev):
+        self.regs = regs
+        self.dev = dev
+
+    def read(self, addr):
+        return self.regs.spi_read(self.dev, addr)
+
+    def write(self, addr, data):
+        self.regs.spi_write(self.dev, addr, data)
+
+    __getitem__ = read
+    __setitem__ = write
+
+
+regs = Regs(REGS)
+
+PLL = SPI(regs, 0)
+ADC = SPI(regs, 1)
+DAC = SPI(regs, 2)
+
+
+__all__ = ['DDR0', 'DDR1', 'DDR0_BUF_LEN', 'regs', 'PLL', 'ADC', 'DAC']
