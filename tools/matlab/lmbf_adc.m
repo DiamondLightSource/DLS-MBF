8d33477382b390ff8e29e8dd06c98419f94a4b24
Michael Abbott <michael.abbott@diamond.ac.uk>
2016-10-05 12:23:55 +0100

    Read matlab data directly from DDR device

--- tools/matlab/lmbf_adc.m
+++ tools/matlab/lmbf_adc.m
@@ -1,14 +1,12 @@
 % Quick and dirty matlab script to capture and read ADC waveform.
-function a = lmbf_adc(blocks)
+function a = lmbf_adc(samples)
 
 reg = [fileparts(mfilename('fullpath')) '/../reg'];
-file_in = '/dev/shm/adc';
 device = '/dev/amc525_lmbf.0.ddr0';
 
-system(sprintf('%s 1 2 %d; dd if=%s count=%d bs=%d status=none of=%s', ...
-    reg, 32768 * blocks, device, blocks, 131072, file_in));
+% 2 samples per capture, plus a bit of overrun
+system(sprintf('%s 1 2 %d', reg, ceil(samples/2) + 16));
 
-f = fopen(file_in);
-a = double(reshape(fread(f, Inf, 'int16=>int16'), 2, [])');
+f = fopen(device);
+a = double(reshape(fread(f, 2*samples, 'int16=>int16'), 2, [])');
 fclose(f);
-delete(file_in);
