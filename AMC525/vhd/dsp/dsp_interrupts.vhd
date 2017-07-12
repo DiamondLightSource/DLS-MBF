796ba7c7e4856310705d1699710650e3ac32866d
Michael Abbott <michael.abbott@diamond.ac.uk>
2017-07-12 09:59:07 +0100

    A little more interrupt pipelining to relax timing
    Looks like we're in a reasonably sensible place for timing closure for the
    time being...

--- AMC525/vhd/dsp/dsp_interrupts.vhd
+++ AMC525/vhd/dsp/dsp_interrupts.vhd
@@ -27,7 +27,7 @@ architecture arch of dsp_interrupts is
     signal seq_start : std_logic_vector(CHANNELS);
     signal interrupts : interrupts_o'SUBTYPE;
 
-    constant INTERRUPT_PIPELINE : natural := 4;
+    constant INTERRUPT_PIPELINE : natural := 8;
 
 begin
     -- Stretch each interrupt pulse so it's not missed by the interrupt
