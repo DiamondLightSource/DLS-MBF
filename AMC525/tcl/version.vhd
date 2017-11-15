c2fc922ac00c8268295eb01500f4b617ee5f1717
Michael Abbott <michael.abbott@diamond.ac.uk>
2017-11-15 15:10:01 +0000

    Remove file that crept in by accident

--- AMC525/tcl/version.vhd
+++
@@ -1,7 +0,0 @@
-package version is
-    constant GIT_VERSION : natural := 16#73400cd#;
-    constant GIT_DIRTY : natural := 1;
-    constant VERSION_MAJOR : natural := 0;
-    constant VERSION_MINOR : natural := 1;
-    constant VERSION_PATCH : natural := 0;
-end;
