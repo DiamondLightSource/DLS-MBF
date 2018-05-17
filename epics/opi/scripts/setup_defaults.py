328490b3ac9ce427dc17c8aad6be2f41357a1969
Benoit Roche <benoit.roche@esrf.fr>
2018-05-17 14:07:56 +0200

    Change default config for ESRF

--- epics/opi/scripts/setup_defaults.py
+++ epics/opi/scripts/setup_defaults.py
@@ -1,25 +1,42 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Defaults and configuration for TMBF
+#  all parameters are shown in this section with a small
+#  description when neeeded
+
+# -- Tune --
+# Tune value is used to:
+#  - generate a suitable FIR
+#  - adjust sweep for tune measurement
+X_tune = 0.44
+Y_tune = 0.39
+
+# -- Feedback parameters --
+
+# dac_out only apply to feedback and does not prevent tune measurement
+# nor cleaning
+dac_output = 0                  # By default Feedback is off
+
+# -- Tune sweep --
+# sweep: harmonic-sweep_range -> harmonic+sweep_range
 
 harmonic = 80
-bunch = 450
-
-# Default tune selection
+sweep_range = 0.05
+tune_reverse = False            # Set to true to sweep backwards
 
 sweep_holdoff = 0
 sweep_dwell_time = 100
+sweep_gain = '-48dB'
 
 blanking_interval = 10000
+keep_feedback = False           # Set to true to enable feedback during sweep
 
-sweep_range = 0.05
-tune_reverse = False            # Set to true to sweep backwards
-keep_feedback = False
-
-dac_output = 0                  # By default DAC output is off
-single_bunch = False            # Set to true for single bunch mode
 detector_input = 'FIR'
 det_gain = '0dB'
 
+single_bunch = False            # Set to true for single bunch mode
+bunch = 450
 
+# -- Tune fit --
 # The following tune fit management parameters are currently not implemented
 tune_select = 'Peak Fit'
 alarm_range = 0.01
@@ -35,15 +52,12 @@ TUNE_sweep_gain = '-42dB'
 AP_sweep_gain = '0dB'
 AP_tune = 0.25
 AP_sweep_range = 0.245
-AP_alarm_range = 0.245
 AP_detector_input = 'ADC'
-AP_min_block_len = 5
 
 # Feedback on
 FB_dac_output = 1               # Enable FIR output in this mode
 FB_keep_feedback = True
 FB_sweep_gain = '-48dB'
-
 
 # Lab setup
 T_tune = 0.7885
