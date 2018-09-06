359824f304c3fd3ddd01775a462312b462e9ec16
Michael Abbott <michael.abbott@diamond.ac.uk>
2018-09-06 09:12:28 +0100

    Catch another fragment of logic between clock domains
    Any logic at all between DSP and ADC clock domains makes synthesis brittle.

--- AMC525/vhd/system/pulse_dsp_to_adc.vhd
+++ AMC525/vhd/system/pulse_dsp_to_adc.vhd
@@ -1,4 +1,5 @@
 -- Converts a DSP control pulse into an ADC pulse.
+-- We pipeline the input to avoid logic between the two clock domains.
 --
 --  adc clk i   /   /   /   /   /   /   /   /   /   /   /   /   /   /
 --                   ___     ___     ___     ___     ___     ___     ___
@@ -6,9 +7,11 @@
 --                   _______
 --  pulse_i    _____/       \___________________________________________
 --                       _______
---  pulse_in   _________/       \_______________________________________
---                       ___
---  pulse_o    _________/   \___________________________________________
+--  pulse_pl   _________/       \_______________________________________
+--                           _______
+--  pulse_in   _____________/       \___________________________________
+--                           ___
+--  pulse_o    _____________/   \_______________________________________
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -25,13 +28,15 @@ entity pulse_dsp_to_adc is
 end;
 
 architecture arch of pulse_dsp_to_adc is
+    signal pulse_pl : std_ulogic := '0';
     signal pulse_in : std_ulogic := '0';
 
 begin
     process (adc_clk_i) begin
         if rising_edge(adc_clk_i) then
-            pulse_in <= pulse_i;
-            pulse_o <= pulse_i and not pulse_in;
+            pulse_pl <= pulse_i;
+            pulse_in <= pulse_pl;
+            pulse_o <= pulse_pl and not pulse_in;
         end if;
     end process;
 end;
