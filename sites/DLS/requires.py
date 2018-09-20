7f352738cc42c30daa7ccbf66fae726a1dc5e049
Michael Abbott <michael.abbott@diamond.ac.uk>
2018-09-20 14:42:33 +0100

    Eliminate most references to dls-python
    Via an extremely scary and site specific hack eliminate nearly all
    references to dls-python from the code base.

--- sites/DLS/requires.py
+++ sites/DLS/requires.py
@@ -1,8 +1,21 @@
 # This file contains the appropriate library requires needed for python at DLS.
 
-from pkg_resources import require
+from pkg_resources import require, VersionConflict, DistributionNotFound
 
-require('numpy==1.11.1')
-require('cothread==2.14')
-require('epicsdbbuilder==1.2')
-require('matplotlib')
+try:
+    require('numpy==1.11.1')
+    require('cothread==2.14')
+    require('epicsdbbuilder==1.2')
+    require('matplotlib')
+
+except (VersionConflict, DistributionNotFound) as e:
+    # The following extremely dirty code is used to automatically switch to the
+    # correct (DLS specific) version of python if any of the requires above
+    # fail.
+    import sys, os
+    if os.path.split(sys.executable)[1] == 'dls-python':
+        # Make sure that we don't end up in an endless loop repeatedly trying to
+        # run dls-python
+        raise e
+    else:
+        os.execvp('dls-python', ['dls-python'] + sys.argv)
