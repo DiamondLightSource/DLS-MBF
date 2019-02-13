DiamondLightSource/DLS-MBF

commit 03b26f7b537b0d9568c2a9fe2aa8ede74af3c13e

Move interpretation of detector shift into detector_core
This is a useful preamble for tune PLL work.

AMC525/vhd/detector/detector_core.vhd
@@ -21,8 +21,7 @@ entity detector_core is
 
         detector_overflow_o : out std_ulogic;
 
-        overflow_mask_i : in signed(95 downto 0);
-        preload_i : in signed(95 downto 0);
+        shift_i : in natural;   -- Valid range 0 to 64
 
         start_i : in std_ulogic;
         write_i : in std_ulogic;
@@ -40,6 +39,9 @@ architecture arch of detector_core is
     signal cos_overflow : std_ulogic;
     signal sin_overflow : std_ulogic;
 
+    signal overflow_mask : signed(95 downto 0);
+    signal preload : signed(95 downto 0);
+
     -- Typically both start_i and write_i are synchronous, and the delay from
     -- start_i to reset data out is 4 ticks, so we need the write delay to be
     -- one less to pick up the data before reset.
@@ -47,6 +49,15 @@ architecture arch of detector_core is
     signal write_in : std_ulogic;
 
 begin
+    -- Compute preload and overflow detection mask for output shift.  The
+    -- preload simply ensures proper rounding by adding 0.5 to the result.
+    preload <= shift_left(to_signed(1, 97), shift_i)(96 downto 1);
+    -- The overflow mask determines which bits will be used for overflow
+    -- detection: we need to include all discarded bits plus our generated sign
+    -- bit, hence the base shift of 65 = 96 - 32 - 1.
+    overflow_mask <= signed(shift_right(not to_unsigned(0, 96), 65 - shift_i));
+
+
     bunch_delay_cos : entity work.dlyreg generic map (
         DLY => 4
     ) port map (
@@ -69,8 +80,8 @@ begin
         mul_i => iq_i.cos,
         enable_i => bunch_enable_cos,
         start_i => start_i,
-        overflow_mask_i => overflow_mask_i,
-        preload_i => preload_i,
+        overflow_mask_i => overflow_mask,
+        preload_i => preload,
         sum_o => iq_out.cos,
         overflow_o => cos_overflow
     );
@@ -81,8 +92,8 @@ begin
         mul_i => iq_i.sin,
         enable_i => bunch_enable_sin,
         start_i => start_i,
-        overflow_mask_i => overflow_mask_i,
-        preload_i => preload_i,
+        overflow_mask_i => overflow_mask,
+        preload_i => preload,
         sum_o => iq_out.sin,
         overflow_o => sin_overflow
     );

AMC525/sim/detector/bench/testbench.vhd
@@ -29,6 +29,7 @@ architecture arch of testbench is
     signal read_data : reg_data_array_t(DSP_DET_REGS);
     signal read_ack : std_ulogic_vector(DSP_DET_REGS);
     signal adc_data : signed(15 downto 0);
+    signal adc_fill_reject : signed(15 downto 0);
     signal fir_data : signed(35 downto 0);
     signal nco_iq : cos_sin_18_t;
     signal window : signed(17 downto 0);
@@ -80,6 +81,7 @@ begin
         read_data_o => read_data,
         read_ack_o => read_ack,
         adc_data_i => adc_data,
+        adc_fill_reject_i => adc_fill_reject,
         fir_data_i => fir_data,
         nco_iq_i => nco_iq,
         window_i => window,

AMC525/sim/detector/do/compile.do
@@ -53,6 +53,8 @@ add wave -group "Body(0) Bunch Mem" \
     sim:detector/detectors(0)/detector_body/bunch_select/bunch_mem/*
 add wave -group "Body(0) Bunch" \
     sim:detector/detectors(0)/detector_body/bunch_select/*
+add wave -group "Body(0) Core" \
+    sim:detector/detectors(0)/detector_body/detector/*
 add wave -group "Body(0) Output" \
     sim:detector/detectors(0)/detector_body/output/*
 add wave -group "Body(0)" sim:detector/detectors(0)/detector_body/*

AMC525/vhd/detector/detector_body.vhd
@@ -51,10 +51,7 @@ architecture arch of detector_body is
     signal iq_shift : cos_sin_96_t;
     signal iq_out : cos_sin_32_t;
 
-    signal shift : integer;
-    signal base_mask : unsigned(95 downto 0) := (others => '1');
-    signal overflow_mask : signed(95 downto 0);
-    signal preload : signed(95 downto 0);
+    signal shift : integer := 0;
 
     signal det_write_dsp_in : std_ulogic;
     signal det_write_dsp : std_ulogic;
@@ -77,11 +74,6 @@ begin
 
     -- Compute preload and overflow detection mask for output shift.
     shift <= 8 * to_integer(output_scaling_i) + 24;
-    preload <= shift_left(96X"0_0000_0000_0000_0000_0000_0001", shift - 1);
-    -- The overflow mask determines which bits will be used for overflow
-    -- detection: we need to include all discarded bits plus our generated sign
-    -- bit, hence the base shift of 65 = 96 - 32 - 1.
-    overflow_mask <= signed(shift_right(base_mask, 65 - shift));
 
 
     -- IQ Detector
@@ -93,8 +85,7 @@ begin
         bunch_enable_i => bunch_enable,
         detector_overflow_o => detector_overflow,
 
-        overflow_mask_i => overflow_mask,
-        preload_i => preload,
+        shift_i => shift,
 
         start_i => start_i,
         write_i => write_i,

commit 0863e50182eb5d67cd6df93359caf2c4a6d5df75

Fix bug in detector overflow mask
This bug was introduced in commit 03b26f7: got the mask inverted!

AMC525/vhd/detector/detector_core.vhd
@@ -59,7 +59,7 @@ begin
     process (shift_i) begin
         for i in 0 to 95 loop
             preload(i) <= to_std_ulogic(i + 1 = shift_i);
-            overflow_mask(i) <= to_std_ulogic(i >= RESULT_WIDTH - 1 + shift_i);
+            overflow_mask(i) <= to_std_ulogic(i < RESULT_WIDTH - 1 + shift_i);
         end loop;
     end process;
 