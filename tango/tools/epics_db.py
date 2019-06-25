7c99ff3ceca507c945ec96f1f8808df223205921
claudioscafuri <claudio.scafuri@elettra.trieste.it>
2019-06-25 14:50:16 +0200

    first build of tango services and configs for Elettra

--- tango/tools/epics_db.py
+++ tango/tools/epics_db.py
@@ -16,6 +16,7 @@ e2t_mode = {
 'mbbi': 'READ_ONLY',
 'mbbo': 'READ_WRITE',
 'stringin': 'READ_ONLY',
+'stringout': 'READ_WRITE',
 'waveform': 'READ_ONLY'}
 
 re_replace_vars = re.compile('(\$\([^\(\)\s]+\))')
@@ -180,7 +181,7 @@ class EPICS_db:
                 pv_type = 'Enum'
             else:
                 pv_type = 'Int'
-        elif dico_db[pv]['__type__'] in ['stringin']:
+        elif dico_db[pv]['__type__'] in ['stringin','stringout']:
             pv_type = 'String'
         elif dico_db[pv]['__type__'] in ['ai', 'ao', 'calc']:
             pv_type = 'Double'
