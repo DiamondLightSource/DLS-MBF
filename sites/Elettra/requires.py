799e016fb3930cc5e46d010059bc954d99790f73
claudioscafuri <claudio.scafuri@elettra.trieste.it>
2019-06-25 14:50:16 +0200

    first build of tango services and configs for Elettra
    Original commit 7c99ff3 restricted to sites only.

--- sites/Elettra/requires.py
+++ sites/Elettra/requires.py
@@ -3,5 +3,5 @@
 from pkg_resources import require, VersionConflict, DistributionNotFound
 
 require('numpy>=1.11.1')
-require('cothread==2.14')
-require('epicsdbbuilder==1.2')
+require('cothread>=2.14')
+#require('epicsdbbuilder>=0.0')
